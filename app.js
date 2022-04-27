7b19ae750d66a31d4806bdaf0e30078553aed88a
Nuuteen <102475649+Nuuteen@users.noreply.github.com>
2022-04-28 01:03:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'));
 
 app.get("/", (req,res) =>{
 
